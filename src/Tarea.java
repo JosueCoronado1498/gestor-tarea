87ff063ee63666e1013d87132b6a264af7738085
josue coronado <coronadojosue435@gmail.com>
2025-05-19 20:33:50 -0300

    primer commit

---
+++ src/Tarea.java
@@ -0,0 +1,70 @@
+public class Tarea {
+
+    private Long id;
+    private String titulo;
+    private String descripcion;
+    private String fechaLimite;
+    private boolean estado;
+
+    public Tarea() {
+    }
+
+    public Tarea(Long id, String titulo, String descripcion, String fechaLimite, boolean estado) {
+        this.id = id;
+        this.titulo = titulo;
+        this.descripcion = descripcion;
+        this.fechaLimite = fechaLimite;
+        this.estado = estado;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getFechaLimite() {
+        return fechaLimite;
+    }
+
+    public void setFechaLimite(String fechaLimite) {
+        this.fechaLimite = fechaLimite;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public String toString() {
+        return "Tarea{" +
+                "id=" + id +
+                ", titulo='" + titulo + '\'' +
+                ", descripcion='" + descripcion + '\'' +
+                ", fechaLimite='" + fechaLimite + '\'' +
+                ", estado=" + estado +
+                '}';
+    }
+}
